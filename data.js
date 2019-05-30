7ace95bc4f5a515db2ac4548209e5034d9b4526f
Hoàng Dư Thanh <hoang.du@codegym.vn>
2019-05-31 01:37:13 +0700

    update case study 0419

---
+++ data.js
@@ -0,0 +1,16 @@
+//C0419G1 - Java - Module 1
+let c0419g1 = '{"games":[' +
+    '{"name":"Save The World","icon":"savetheworld.png","link":"casestudy-savetheworld-elnino228","author":"Đào Văn Đạt" },' +
+    '{"name":"Escaping Tank","icon":"escapingtank.ico","link":"non-complete-escapingtank","author":"Nguyễn Tuấn Anh"},' +
+    '{"name":"Bouncing Ball","icon":"bouncingball.png","link":"gamedongian","author":"Trần Thanh Hiếu Thảo"},' +
+    '{"name":"Black Jack","icon":"blackjack.png","link":"goofy-hypatia-07e3b0","author":"Phạm Văn Lâm"},' +
+    '{"name":"Tank 92","icon":"tank92.png","link":"tank92","author":"Đỗ Phú Hưởng" },' +
+    '{"name":"Racing Game","icon":"racing.jpg","link":"gifted-poincare-ac3cbd","author":"Khánh Nguyễn"},' +
+    '{"name":"Break Brick","icon":"breakbrick.png","link":"game-bong-lay","author":"Phạm Văn Lâm"},' +
+    '{"name":"Galaxy Adventure","icon":"default.png","link":"c0419g1thuan","author":"Nguyễn Đức Thuận"}]}';
+
+//C0419H1 - PHP - Module 1
+let c0419h1 = '{"games":[' +
+    '{"name":"Snake vs Ball","icon":"snakevsball.png","link":"snake-vs-ball","author":"Vũ Xuân Quý" },' +
+    '{"name":"Matching Game","icon":"matchinggame.png","link":"linhnv296","author":"Nguyễn Văn Linh"},' +
+    '{"name":"Troll Ball","icon":"trollball.jpg","link":"trollball","author":"Lê Văn Dương" }]}';
